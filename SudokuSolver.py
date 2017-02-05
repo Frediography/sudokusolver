0a00a23a962d51db33dcbb947fb5f7adac87a1e2
Frediography <fredflack@gmail.com>
2017-02-05 22:02:28 +0000

    vertical branch
    vertical branch with the horizontal stuff neglected for now.

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -16,7 +16,7 @@ while x <= (9*9)-1: # This just tells it when to stop looping
 print("- " * 100)
 x = 0 # reset our counters
 y = 0 # reset our counters
-
+"""
 # Then; ask the user what the blanks are represented as, and make it a variable:
 b = "0" # Use the following for input: input("What are the blank spaces represented as in this puzzle? ")
 # Let's find the blanks and put all possible integers in, before starting to look at other numbers
@@ -36,7 +36,7 @@ if debug == 1:
     print("START WITH ROW 0: ")
 
 # Function to find the numbers that could be on each blank on the horizontal (based just on that line)
-def horicheck(l):
+def check(l):
     x = 0 # define a variable for the counter
     h = l + 9 # define a variable for the Higher of the range
     y = 1
@@ -66,28 +66,65 @@ def horiadd(x, l):
         l += 1
 
 # Now execute the functions:
-x = 0
-q = x
-e = x
-while x <= 9*8:
-    if q not in note:
-        q += 1
+x = 0 # reset the counter
+e = x # e is used as the index number of the next blank in the 'note' variable
+q = x # q is used as another counter, but this time it's for the 'note' variable; so IF the number at the start of a row isn't a blank; we can check for the next number.
+while x <= 9*8: # while x is in low enough to look at the rows
+    if q not in note: # if the number at the start of the row isn't blank - look for the next plank
+        q += 1 # by adding 1 to q
     else:
-        e = int(note.index(q))
-        horicheck(x)
-        horiadd(x, e)
-        possibles =[]
-        x += 9
-        q = x
-        d = (note[e]/9) + 1
+        e = int(note.index(q)) # use the integer!
+        check(x) # call the function defined earlier
+        horiadd(x, e) # call the other function defined earlier
+        possibles = [] # clear our the possible number variable each time after you've added the numbers
+        x += 9 # go to the next row
+        q = x # refresh q to be the number of the start of the next row
+        d = (note[e]/9) + 1 # d is just to say what row we are starting on next
         if debug == 1:
             print("- " * 100)
         if int(d) < 9:
             if debug == 1:
                 print("MOVING TO ROW: " + str(d))
         else:
-            break
+            break # finish"""
 
 ####
 #### NOW MOVE ONTO VERTICALS
 ####
+"""def verticheck (l):
+        x = 0 # define a variable for the counter
+        y = 1
+        while x <= 8: # to make sure we stay on one horizontal line
+            if str(y) not in puzzle[l:h]: # if the number isn't in the line, it's a possible
+                possibles.append(n[x])
+                x += 1
+                y += 1
+            else: # if the number is in the line, it's not a possible.
+                x += 1 # so just move on to the next one
+                y += 1
+        if debug == 1:
+            print("the possibilities for this row, based just on this row, are: " + str(possibles))
+    """
+### This is looking at the columns/verticals
+
+c = 0 # this is a variable for the column number
+notpossibles = [] # create a not - possibles variable
+possibles = [] # empty the possibles variable
+for c in range (0, 9, 1): # create a loop to do this 9 times over
+    notpossibles = [] # clear out the not possibles variable for the following loop
+    possibles = [] # clear out the possibles variable for the following loop
+    for c in range (c, 81, 9): # this is the loop to ensure we only look at the vericals together
+        for x in range(1, 10, 1): # this loop is each integer we are looking for on the vertical
+            if str(x) in puzzle[c]: # if the x is in the puzzle at the point (c) we are looking at
+                #if debug == 1:
+                #    print(str(x) + " is in the puzzle at point: " + str(c))
+                notpossibles.append(x) # add the number that is in the column to the not possibles
+    if debug == 1:
+        print("The following numbers are on column " + str(c-72) + ": " + str(notpossibles))
+        for x in range(1, 10, 1): # loop to add possibles
+            if x not in notpossibles: # if the number isn't in the not-possibles
+                possibles.append(x) # add the number to the possibles variable
+        if debug == 1:
+            print("Leaving the following numbers as possibles: " + str(possibles))
+    if debug == 1:
+        print("- " *50)
